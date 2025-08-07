c7982790fe146bdc66a9c8a3510d8aede2e9336c
nico-gao <jtgao99@gmail.com>
2025-08-07 11:00:10 -0400

    Update logger.js

--- src/middlewares/logger.js
+++ src/middlewares/logger.js
@@ -1,7 +1,7 @@
 const logger = (store) => (next) => (action) => {
   console.group(action.type);
   console.info("dispatching", action);
-  let result = next(action); // <- original store.dispatch function
+  let result = next(action); // <- next middleware or the original store.dispatch function
   console.log("next state", store.getState());
   console.groupEnd();
   return result;
